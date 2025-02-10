589a6852ad239c5220160acf8640a6ee76ba4e6c
Naijth <163039373+Naijth@users.noreply.github.com>
2025-02-10 16:36:42 +0100

    ciklus bevezetese

--- main.js
+++ main.js
@@ -51,60 +51,19 @@ th3.innerHTML = array[0].love1; // changes the innerHTML of the th3 variable to 
 const tbody = document.createElement('tbody'); // creates the tbody variable and assigns it a tbody HTML element
 table.appendChild(tbody); // appends the tbody variable to the table variable
 
-const trB1 = document.createElement('tr'); // creates the trB1 variable and assigns it a tr HTML element
-tbody.appendChild(trB1); // appends the trB1 variable to the tbody variable
-const td1 = document.createElement('td'); // creates the td1 variable and assigns it a td HTML element
-trB1.appendChild(td1); // appends the td1 variable to the trB1 variable
-td1.innerHTML = array[1].name; // changes the innerHTML of the td1 to the stated variable
-const td2 = document.createElement('td'); // creates the td2 variable and assigns it a td HTML element
-trB1.appendChild(td2); // appends the td2 variable to the trB1 variable
-td2.innerHTML = array[1].age; // changes the innerHTML of the td2 variable to the stated variable
-const td3 = document.createElement('td'); // creates the td3 variable and assigns it a td HTML element
-trB1.appendChild(td3); // appends the td3 variable to the trB1 variable
-td3.innerHTML = array[1].love1; // changes the innerHTML of the td3 variable to the stated variable
-const td4 = document.createElement('td'); // creates the td4 variable and assigns it a td HTML element
-trB1.appendChild(td4); // appends the td4 variable to the trB1 variable
-td4.innerHTML = array[1].love2; // changes the innerHTML of the td4 variable to the stated variable
-
-const trB2 = document.createElement('tr'); // creates the trB2 variable and assigns it a tr HTML element
-tbody.appendChild(trB2); // appends the trB2 variable to the tbody variable
-const td5 = document.createElement('td'); // creates the td5 variable and assigns it a td HTML element
-trB2.appendChild(td5); // appends the td5 variable to the trB2 variable
-td5.innerHTML = array[2].name;  // changes the innerHTML of the td5 variable to the stated variable
-const td6 = document.createElement('td'); // creates the td6 variable and assigns it a td HTML element
-trB2.appendChild(td6); // appends the td6 variable to the trB2 variable
-td6.innerHTML = array[2].age; // changes the innerHTML of the td6 variable to the stated variable
-const td7 = document.createElement('td'); // creates the td7 variable and assigns it a td HTML element
-trB2.appendChild(td7); // appends the td7 variable to the trB2 variable
-td7.colspan = 2; // assigns the td7 variable the colspan attribute and sets it to 2
-td7.innerHTML = array[2].love1; // changes the innerHTML of the td7 variable to the stated variable
-
-const trB3 = document.createElement('tr'); // creates the trB3 variable and assigns it a tr HTML element
-tbody.appendChild(trB3); // appends the trB3 variable to the tbody variable
-const td8 = document.createElement('td'); // creates the td8 variable and assigns it a td HTML element
-trB3.appendChild(td8); // appends the td8 variable to the trB3 variable
-td8.innerHTML = array[3].name; // changes the innerHTML of the td8 variable to the stated variable
-const td9 = document.createElement('td'); // creates the td9 variable and assigns it a td HTML element
-trB3.appendChild(td9); // appends the td9 variable to the trB3 variable
-td9.innerHTML = array[3].age; // changes the innerHTML of the td9 variable to the stated variable
-const td10 = document.createElement('td'); // creates the td10 variable and assigns it a td HTML element
-trB3.appendChild(td10); // appends the td10 variable to the trB3 variable
-td10.innerHTML = array[3].love1; // changes the innerHTML of the td10 variable to the stated variable
-const td11 = document.createElement('td'); // creates the td11 variable and assigns it a td HTML element
-trB3.appendChild(td11); // appends the td11 variable to the trB3 variable
-td11.innerHTML = array[3].love2; // changes the innerHTML of the td11 variable to the stated variable
-
-const trB4 = document.createElement('tr'); // creates the trB4 variable and assigns it a tr HTML element
-tbody.appendChild(trB4); // appends the trB4 variable to the tbody variable
-const td12 = document.createElement('td'); // creates the td12 variable and assigns it a td HTML element
-trB4.appendChild(td12); // appends the td12 variable to the trB4 variable
-td12.innerHTML = array[4].name; // changes the innerHTML of the td12 variable to the stated variable
-const td13 = document.createElement('td'); // creates the td13 variable and assigns it a td HTML element
-trB4.appendChild(td13); // appends the td13 variable to the trB4 variable
-td13.innerHTML = array[4].age; // changes the innerHTML of the td13 variable to the stated variable
-const td14 = document.createElement('td'); // creates the td14 variable and assigns it a td HTML element
-trB4.appendChild(td14); // appends the td14 variable to the trB4 variable
-td14.innerHTML = array[4].love1; // changes the innerHTML of the td14 variable to the stated variable
-const td15 = document.createElement('td'); // creates the td15 variable and assigns it a td HTML element
-trB4.appendChild(td15); // appends the td15 variable to the trB4 variable
-td15.innerHTML = array[4].love2; // changes the innerHTML of the td15 variable to the stated variable
+for (let i = 1; i < array.length; i++) { // for loop that increments for each by one, goes until i is smaller than the length of array
+    const tr = document.createElement('tr'); // creates the tr variable and assigns it a tr HTML element
+    tbody.appendChild(tr); // appends the tr variable to the tbody variable
+    const td1 = document.createElement('td'); // creates the td1 variable and assigns it a td HTML element
+    tr.appendChild(td1); // appends the td1 variable to the tr variable
+    td1.innerHTML = array[i].name; // changes the innerHTML of the td1 to the i'th variable
+    const td2 = document.createElement('td'); // creates the td2 variable and assigns it a td HTML element
+    tr.appendChild(td2); // appends the td2 variable to the tr variable
+    td2.innerHTML = array[i].age; // changes the innerHTML of the td2 variable to the i'th variable
+    const td3 = document.createElement('td'); // creates the td3 variable and assigns it a td HTML element
+    tr.appendChild(td3); // appends the td3 variable to the tr variable
+    td3.innerHTML = array[i].love1; // changes the innerHTML of the td3 variable to the i'th variable
+    const td4 = document.createElement('td'); // creates the td4 variable and assigns it a td HTML element
+    tr.appendChild(td4); // appends the td4 variable to the tr variable
+    td4.innerHTML = array[i].love2; // changes the innerHTML of the td4 variable to the i'th variable
+}
