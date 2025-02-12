abfd7d782d268f2008dcdd8f85dd89d924f465b5
Naijth <163039373+Naijth@users.noreply.github.com>
2025-02-12 20:42:15 +0100

    tabla fejlec generalas bevezetese

--- main.js
+++ main.js
@@ -31,56 +31,50 @@ const array = [ //creates an array named array
 
 const table = document.createElement('table'); // creates the table variable and assigns it a table HTML element
 document.body.appendChild(table); // appends the table variable to the document's body
-
-function renderTableHead(){
-    const thead = document.createElement('thead'); // creates the thead variable and assigns it a thead HTML element
-    table.appendChild(thead); // appends the thead variable to the table variable
-
-    const trH = document.createElement('tr'); // creates the trH variable and assigns it a tr HTML element
-    thead.appendChild(trH); // appends the trH variable to the thead variable
-    const th1 = document.createElement('th'); // creates the th1 variable and assigns it a th HTML element
-    trH.appendChild(th1); // appends the th1 variable to the trH variable
-    th1.innerHTML = array[0].name; // changes the innerHTML of the th1 variable to the stated variable
-    const th2 = document.createElement('th'); // creates the th2 variable and assigns it a th HTML element
-    trH.appendChild(th2); // appends the th2 variable to the trH variable
-    th2.innerHTML = array[0].age; // changes the innerHTML of the th2 variable to the stated variable
-    const th3 = document.createElement('th'); // creates the th3 variable and assigns it a th HTML element
-    trH.appendChild(th3); // appends the th3 variable to the trH variable
-    th3.colSpan = "2"; // assigns the th3 variable the colspan attribute and sets it to 2
-    th3.innerHTML = array[0].love1; // changes the innerHTML of the th3 variable to the stated variable
-}
-
+const thead = document.createElement('thead'); // creates the thead variable and assigns it a thead HTML element
+table.appendChild(thead); // appends the thead variable to the table variable
 const tbody = document.createElement('tbody'); // creates the tbody variable and assigns it a tbody HTML element
 table.appendChild(tbody); // appends the tbody variable to the table variable
 
-function renderTableBody(){ // creates the renderTableBody function
+function renderTable(){ // creates the renderTableBody function
     for (let i = 0; i < array.length; i++) { // for loop that increments for each by one, goes until i is smaller than the length of array
-        if (i = 0) {
-            
-        }
-        const tr = document.createElement('tr'); // creates the tr variable and assigns it a tr HTML element
-        tbody.appendChild(tr); // appends the tr variable to the tbody variable
-        const td1 = document.createElement('td'); // creates the td1 variable and assigns it a td HTML element
-        tr.appendChild(td1); // appends the td1 variable to the tr variable
-        td1.innerHTML = array[i].name; // changes the innerHTML of the td1 to the i'th variable
-        const td2 = document.createElement('td'); // creates the td2 variable and assigns it a td HTML element
-        tr.appendChild(td2); // appends the td2 variable to the tr variable
-        td2.innerHTML = array[i].age; // changes the innerHTML of the td2 variable to the i'th variable
-        const td3 = document.createElement('td'); // creates the td3 variable and assigns it a td HTML element
-        tr.appendChild(td3); // appends the td3 variable to the tr variable
-        td3.innerHTML = array[i].love1; // changes the innerHTML of the td3 variable to the i'th variable
-        if (array[i].love2 == undefined) { // checks if love2 is undefined
-            td3.colSpan = "2"; // if yes, td3's colspan is now 2
-        } else { // otherwise...
-            const td4 = document.createElement('td'); // creates the td4 variable and assigns it a td HTML element
-            tr.appendChild(td4); // appends the td4 variable to the tr variable
-            td4.innerHTML = array[i].love2; // changes the innerHTML of the td4 variable to the i'th variable
+        if (i == 0) {
+            const trH = document.createElement('tr'); // creates the trH variable and assigns it a tr HTML element
+            thead.appendChild(trH); // appends the trH variable to the thead variable
+            const th1 = document.createElement('th'); // creates the th1 variable and assigns it a th HTML element
+            trH.appendChild(th1); // appends the th1 variable to the trH variable
+            th1.innerHTML = array[i].name; // changes the innerHTML of the th1 variable to the stated variable
+            const th2 = document.createElement('th'); // creates the th2 variable and assigns it a th HTML element
+            trH.appendChild(th2); // appends the th2 variable to the trH variable
+            th2.innerHTML = array[i].age; // changes the innerHTML of the th2 variable to the stated variable
+            const th3 = document.createElement('th'); // creates the th3 variable and assigns it a th HTML element
+            trH.appendChild(th3); // appends the th3 variable to the trH variable
+            th3.colSpan = "2"; // assigns the th3 variable the colspan attribute and sets it to 2
+            th3.innerHTML = array[i].love1; // changes the innerHTML of the th3 variable to the stated variable
+        } else {
+            const tr = document.createElement('tr'); // creates the tr variable and assigns it a tr HTML element
+            tbody.appendChild(tr); // appends the tr variable to the tbody variable
+            const td1 = document.createElement('td'); // creates the td1 variable and assigns it a td HTML element
+            tr.appendChild(td1); // appends the td1 variable to the tr variable
+            td1.innerHTML = array[i].name; // changes the innerHTML of the td1 to the i'th variable
+            const td2 = document.createElement('td'); // creates the td2 variable and assigns it a td HTML element
+            tr.appendChild(td2); // appends the td2 variable to the tr variable
+            td2.innerHTML = array[i].age; // changes the innerHTML of the td2 variable to the i'th variable
+            const td3 = document.createElement('td'); // creates the td3 variable and assigns it a td HTML element
+            tr.appendChild(td3); // appends the td3 variable to the tr variable
+            td3.innerHTML = array[i].love1; // changes the innerHTML of the td3 variable to the i'th variable
+            if (array[i].love2 == undefined) { // checks if love2 is undefined
+                td3.colSpan = "2"; // if yes, td3's colspan is now 2
+            } else { // otherwise...
+                const td4 = document.createElement('td'); // creates the td4 variable and assigns it a td HTML element
+                tr.appendChild(td4); // appends the td4 variable to the tr variable
+                td4.innerHTML = array[i].love2; // changes the innerHTML of the td4 variable to the i'th variable
+            }
         }
     } 
 }
 
-renderTableHead(); // renders the table head
-renderTableBody(); //renders the table body
+renderTable(); //renders the table body
 
 const form = document.getElementById('form') // creating a variable and putting the htmlElements
 
